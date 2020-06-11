61b59b2c69faddfe549b9d16e2840acb464ce967
David <zenoah82000@gmail.com>
2020-06-11 16:24:46 +0800

    補上eslink檔案

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,16 +3,16 @@ import '../styles/home.scss'
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 import { Link, NavLink, withRouter } from 'react-router-dom'
 
-
 function Home(props) {
-
-
   return (
     <>
       <div className="banner">
-        
+        <div className="container">
+          <div className="searchbar">
+            <div className="btnList"></div>
+          </div>
+        </div>
       </div>
-
     </>
   )
 }
