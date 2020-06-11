6e0b8b68a291eda6cb26d75da212bedc618731d2
asd24971234 <65526219+asd24971234@users.noreply.github.com>
2020-06-11 11:01:57 +0800

    Revert "server"
    This reverts commit 535790d081ee70b08d707402313df18e44e4c4dc.

--- server/src/http_server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server= http.createServer((req,res)=>{
-    res.writeHead(404,{
-        'Content-Type':'text/html'
-    });
-    res.end(`<h2>${console.log(req)}</h2>`)
-
-});
-server.listen(3000);
