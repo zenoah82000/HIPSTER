cc0bbf5b7e4614f6e789cdf19f85f4d861093def
David <zenoah82000@gmail.com>
2020-06-12 11:53:26 +0800

    0612

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,9 +10,28 @@ function Home(props) {
         <div className="container">
           <div className="searchbar">
             <div className="btnList">
-              <div className="btn active">地點搜尋</div>
-              <div className="btn">活動名稱</div>
-              <div className="btn">活動地點</div>
+              <div id="location" className="btn active">
+                地點搜尋
+              </div>
+              <div id="activename" className="btn">
+                活動名稱
+              </div>
+              <div id="time" className="btn">
+                活動時間
+              </div>
+            </div>
+            <div id="location-search">
+              <select>
+                <option>台北市</option>
+                <option>台中市</option>
+                <option>高雄市</option>
+              </select>
+              <select>
+                <option>內湖區</option>
+                <option>中正區</option>
+                <option>大同區</option>
+              </select>
+              <input type="text" placeholder="請輸入活動地址...." />
             </div>
           </div>
         </div>
