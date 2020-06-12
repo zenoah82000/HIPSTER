4a2cbd7995f7c9389f0b6c8060d9b5b8ba830fc4
asd24971234 <65526219+asd24971234@users.noreply.github.com>
2020-06-12 11:34:55 +0800

    bo

---
+++ client/src/pages/ShoppingCar.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import '../styles/ShoppingCar.scss'
+
+
+
+function ShoppingCar() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+export default ShoppingCar
+
