ad4f1e7271796afd7dcf11c8ee28d570475c593d
asd24971234 <65526219+asd24971234@users.noreply.github.com>
2020-06-12 11:52:29 +0800

    Update ShoppingCar.js

--- client/src/pages/ShoppingCar.js
+++ client/src/pages/ShoppingCar.js
@@ -8,7 +8,17 @@ function ShoppingCar() {
 
   return (
     <>
-      
+      <div className="container d-flex">
+                <input type="checkbox" name="checkall" id="checkall" />
+                <label for="checkall">全選</label>
+                <div className="col">
+                  <h6>數量</h6>
+                </div>
+                <div className="col">
+                  <h6>金額</h6>
+                </div>
+            </div>
+    
     </>
   )
 }
