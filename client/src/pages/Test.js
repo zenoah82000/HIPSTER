a052c4948493b3bed4674676eba7c95dc3d8e016
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2020-06-12 16:44:22 +0800

    sidebar

---
+++ client/src/pages/Test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Sidebar from '../components/member/Sidebar'
+
+// import '../../styles/sidebar.scss'
+
+function Test(){
+    return<>
+      
+                    <Sidebar/>
+                
+    </>
+}
+
+export default Test
