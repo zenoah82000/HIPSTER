e2fb070756a0cb03f8d1d2e7d78eb5a68e55e0b3
Jeanzulin <jeanzu19@gmail.com>
2020-06-12 14:06:51 +0800

    Update Map.js

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -6,17 +6,7 @@ import { Link, NavLink, withRouter } from 'react-router-dom'
 function Map(props) {
   return (
     <>
-      <div className="banner">
-        <div className="container">
-          <div className="searchbar">
-            <div className="btnList">
-              <div className="btn active">地點搜尋</div>
-              <div className="btn">活動名稱</div>
-              <div className="btn">活動地點</div>
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </>
   )
 }
