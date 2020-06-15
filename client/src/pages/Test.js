370081839695379b488680af6ea1be4819d4dc41
Poseidoncode <41856126+ryan1095@users.noreply.github.com>
2020-06-15 08:01:58 +0800

    修改sidebar
    修改sidebar

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -4,12 +4,16 @@ import Sidebar from '../components/member/Sidebar'
 
 // import '../../styles/sidebar.scss'
 
-function Test(){
-    return<>
-      
-                    <Sidebar/>
-                
+function Test() {
+  return (
+    <>
+      <Sidebar>
+        <div id="time" className="btn">
+          活動時間
+        </div>
+      </Sidebar>
     </>
+  )
 }
 
 export default Test
