8aa9817c7cea4be0d28de2c208a0475df2a4d5fd
Jeanzulin <jeanzu19@gmail.com>
2020-06-15 17:34:17 +0800

    payment

---
+++ client/src/pages/payment/paymentFinish.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//引入自訂元件
+import paymentTotal from '../../components/payment/paymentTotal'
+
+function paymentFinish(props) {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            
+          </div>
+          <div className="col-3">
+            <paymentTotal/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default paymentFinish
---
+++ client/src/pages/payment/paymentType.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//引入自訂元件
+import paymentTotal from '../../components/payment/paymentTotal'
+
+function paymentType(props) {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className=""></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default paymentType
