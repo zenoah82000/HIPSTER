0dbc7de9572aa41fc6d19f9ef20462b0bec34dc3
Martin <l850614@gmail.com>
2020-06-16 22:13:00 +0800

    add component AsideBar

---
+++ client/src/components/product/AsideBar.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+function AsideBar(props) {
+  return (
+    <>
+      <aside className="aside-wrapper col-md-3">
+        <div className="aside-wrapper-filter-box">
+          <h3>所有商品類別</h3>
+          <div>
+            <div className="drop-title">
+              <h4>戶外活動</h4>
+            </div>
+            <ul className="checkbox-dropdown-list">
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+            </ul>
+          </div>
+          <div>
+            <div className="drop-title">
+              <h4>戶外活動</h4>
+            </div>
+            <ul className="checkbox-dropdown-list">
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+            </ul>
+          </div>
+          <div>
+            <div className="drop-title">
+              <h4>戶外活動</h4>
+            </div>
+            <ul className="checkbox-dropdown-list active">
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+            </ul>
+          </div>
+          <div>
+            <div className="drop-title">
+              <h4>戶外活動</h4>
+            </div>
+            <ul className="checkbox-dropdown-list">
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+              <li className="checkbox">1</li>
+            </ul>
+          </div>
+        </div>
+        <div className="aside-wrapper-filter-box">
+          <h3>導覽語言</h3>
+          <ul className="checkbox-dropdown-list">
+            <li className="type-li active">全部</li>
+            <li className="type-li active">中文</li>
+            <li className="type-li active">English</li>
+            <li className="type-li active">日本語</li>
+          </ul>
+        </div>
+        <div className="aside-wrapper-filter-box">
+          <h3>行程時間</h3>
+          <ul className="checkbox-dropdown-list">
+            <li className="type-li active">全部</li>
+            <li className="type-li active">中文</li>
+            <li className="type-li active">English</li>
+            <li className="type-li active">日本語</li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  )
+}
+
+export default AsideBar
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,78 +1,14 @@
 import React from 'react'
 import '../styles/Product.scss'
 
+import AsideBar from '../components/product/AsideBar'
+
 function Product(props) {
   return (
     <>
       <div className="container product-content">
         <div className="row">
-          <aside className="aside-wrapper col-md-3">
-            <div className="aside-wrapper-filter-box">
-              <h3>所有商品類別</h3>
-              <div>
-                <div className="drop-title">
-                  <h4>戶外活動</h4>
-                </div>
-                <ul className="checkbox-dropdown-list">
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                </ul>
-              </div>
-              <div>
-                <div className="drop-title">
-                  <h4>戶外活動</h4>
-                </div>
-                <ul className="checkbox-dropdown-list">
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                </ul>
-              </div>
-              <div>
-                <div className="drop-title">
-                  <h4>戶外活動</h4>
-                </div>
-                <ul className="checkbox-dropdown-list active">
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                </ul>
-              </div>
-              <div>
-                <div className="drop-title">
-                  <h4>戶外活動</h4>
-                </div>
-                <ul className="checkbox-dropdown-list">
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                  <li className="checkbox">1</li>
-                </ul>
-              </div>
-            </div>
-            <div className="aside-wrapper-filter-box">
-              <h3>導覽語言</h3>
-              <ul className="checkbox-dropdown-list">
-                <li className="type-li active">全部</li>
-                <li className="type-li active">中文</li>
-                <li className="type-li active">English</li>
-                <li className="type-li active">日本語</li>
-              </ul>
-            </div>
-            <div className="aside-wrapper-filter-box">
-              <h3>行程時間</h3>
-              <ul className="checkbox-dropdown-list">
-                <li className="type-li active">全部</li>
-                <li className="type-li active">中文</li>
-                <li className="type-li active">English</li>
-                <li className="type-li active">日本語</li>
-              </ul>
-            </div>
-          </aside>
+          <AsideBar />
           <main className="col-md-9 product-list">
             <div className="product-result">
               <h4>搜索結果</h4>
