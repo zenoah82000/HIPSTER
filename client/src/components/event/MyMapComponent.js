8f80a2ac8aca7d49adb514eb1e2d12a21b68ad7f
Jeanzulin <jeanzu19@gmail.com>
2020-06-16 16:24:54 +0800

    map

--- client/src/components/event/MyMapComponent.js
+++ client/src/components/event/MyMapComponent.js
@@ -1,71 +1,123 @@
-import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-// import Markers from '@somarmeteorologia/react-leaflet-markers'
+import React from 'react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { GiCoffeeCup } from 'react-icons/gi'
 
+import CafeData from '../../data/cafe.json'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import { FaRegClock } from 'react-icons/fa'
+import { FaRegCalendarCheck } from 'react-icons/fa'
 
 const MyMapComponent = () => {
-    const [activePark, setActivePark] = React.useState(null);
+  const [activeData, setActiveData] = React.useState(null)
 
-    const parkData = {
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {
-                    "PARK_ID": 960,
-                    "NAME": "Bearbrook Skateboard Park",
-                    "DESCRIPTIO": "Flat asphalt surface, 5 components"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [25.0135914, 121.2332019]
-                }
-            },
-            {
-                "type": "Feature",
-                "properties": {
-                    "PARK_ID": 1219,
-                    "NAME": "Bob MacQuarrie Skateboard Park (SK8 Extreme Park)",
-                    "DESCRIPTIO": "Flat asphalt surface, 10 components, City run learn to skateboard programs, City run skateboard camps in summer"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [-75.546518086577947, 45.467134581917357]
-                }
-            }
-        ]
+  const data = {
+    //name{
+    //[lat,lag]
+    //address
+    //star
+    //}
+  }
+
+  CafeData.cafes.forEach((item, i) => {
+    data[item.name] = {
+      id: item.id,
+      lat: item.latitude,
+      log: item.longitude,
+      star: item.score,
     }
-    return (
-        <div  className="">
-        <Map center={[25.0338438, 121.543350]} zoom={20}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {parkData.features.map(park => (
-                <Marker
-                    // markers={
-                    //     markers.map(marker => (
-                    //         <Marker
-                    //             key={marker.key}
-                    //             icon={defaultIcon}
-                    //             position={[marker.latitude, marker.longitude]}
-                    //             properties={marker}   />
-                    //             ))
-                    //           }
-                        key={park.properties.PARK_ID}
-                        position={[
-                                    25.0338438
-                                    , 121.543321
-                                ]}
-                        onClick={() => {
-                                    setActivePark(park);
-                                }}
-                            />
-                        ))}
-      </Map>
-</div>
+  })
 
-    );
+  let dataArry = []
+
+  for (const key in data) {
+    data[key].name = key
+    dataArry.push(data[key])
+  }
+
+  console.log(dataArry)
+  console.log(data)
+
+  return (
+    <div className="">
+      <Map center={[25.0338438, 121.54335]} zoom={20}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {/* {data.map((data) => {
+
+          ;<Marker position={[data.lat, data.log]} />
+        })} */}
+
+        {dataArry.map((item) => (
+          <Marker
+            // markers={
+            //     markers.map(marker => (
+            //         <Marker
+            //             key={marker.key}
+            //             icon={defaultIcon}
+            //             position={[marker.latitude, marker.longitude]}
+            //             properties={marker}   />
+            //             ))
+            //           }
+            // key={data.id}
+            position={[item.lat, item.log]}
+            onClick={() => {
+              setActiveData(data)
+            }}
+          >
+            <Popup>
+              <div style={{ width: '500px', height: '500px' }}>
+                <h5>
+                  <GiCoffeeCup />
+                  {item.name}
+                </h5>
+                <ul className=" list-unstyled">
+                  <li>
+                    <div className="mapCategory">手作課程</div>
+                  </li>
+                  <li>星等{item.star}</li>
+                  <li>
+                    <span className="mr-2">
+                      <FaRegClock />
+                    </span>
+                    營業時間
+                  </li>
+                  <li>
+                    <span className="mr-2">
+                      <FaMapMarkerAlt />
+                    </span>
+                    地點
+                  </li>
+                  <li>
+                    <span className="mr-2">
+                      <FaRegCalendarCheck />
+                    </span>
+                    活動日期：
+                  </li>
+                </ul>
+                <div
+                  style={{
+                    width: '300px',
+                    height: '300px',
+                    objectFit: 'cover',
+                    overflow: 'hidden',
+                    marginTop: '50px',
+                  }}
+                >
+                  <img
+                    src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+    </div>
+  )
 }
 
 export default MyMapComponent
