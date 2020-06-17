a3df2d767a7d3443ecccfa0481221250345c66b9
Yu-Chia <l850614@gmail.com>
2020-06-17 17:29:15 +0800

    edit asidebar product&scss

--- client/src/components/product/AsideBar.js
+++ client/src/components/product/AsideBar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Dropdown } from 'react-bootstrap'
+import Calendar from 'react-calendar'
 
 function AsideBar(props) {
   return (
@@ -8,7 +10,7 @@ function AsideBar(props) {
           <h3>所有商品類別</h3>
           <div>
             <div className="drop-title">
-              <h4>戶外活動</h4>
+              <h5>戶外活動</h5>
             </div>
             <ul className="checkbox-dropdown-list">
               <li className="checkbox">1</li>
@@ -19,7 +21,7 @@ function AsideBar(props) {
           </div>
           <div>
             <div className="drop-title">
-              <h4>戶外活動</h4>
+              <h5>戶外活動</h5>
             </div>
             <ul className="checkbox-dropdown-list">
               <li className="checkbox">1</li>
@@ -30,7 +32,7 @@ function AsideBar(props) {
           </div>
           <div>
             <div className="drop-title">
-              <h4>戶外活動</h4>
+              <h5>戶外活動</h5>
             </div>
             <ul className="checkbox-dropdown-list active">
               <li className="checkbox">1</li>
@@ -41,7 +43,7 @@ function AsideBar(props) {
           </div>
           <div>
             <div className="drop-title">
-              <h4>戶外活動</h4>
+              <h5>戶外活動</h5>
             </div>
             <ul className="checkbox-dropdown-list">
               <li className="checkbox">1</li>
@@ -51,6 +53,12 @@ function AsideBar(props) {
             </ul>
           </div>
         </div>
+        <Dropdown>
+          <Dropdown.Toggle className="aside-wrapper-filter-box" drop={'down'}>
+            <h3>篩選出發日期</h3>
+          </Dropdown.Toggle>
+          <Dropdown.Menu></Dropdown.Menu>
+        </Dropdown>
         <div className="aside-wrapper-filter-box">
           <h3>導覽語言</h3>
           <ul className="checkbox-dropdown-list">
@@ -63,10 +71,10 @@ function AsideBar(props) {
         <div className="aside-wrapper-filter-box">
           <h3>行程時間</h3>
           <ul className="checkbox-dropdown-list">
-            <li className="type-li active">全部</li>
-            <li className="type-li active">中文</li>
-            <li className="type-li active">English</li>
-            <li className="type-li active">日本語</li>
+            <li className="type-li active">0~1小時</li>
+            <li className="type-li active">1~3小時</li>
+            <li className="type-li active">3~5小時</li>
+            <li className="type-li active">5小時以上</li>
           </ul>
         </div>
       </aside>
