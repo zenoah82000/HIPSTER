432270acead460fa9ee43338d81178135bb7e6e5
Jeanzulin <jeanzu19@gmail.com>
2020-06-17 15:51:15 +0800

    Update Map.js

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -6,15 +6,14 @@ import EventMapDataList from '../components/event/EventMapDataList'
 import EventSearchBar from '../components/event/EventSearchBar'
 import MyMapComponent from '../components/event/MyMapComponent'
 
-
 function Map(props) {
   return (
     <>
       <div className="row">
         <div className="col-4">
-          <button type="button" >測試用</button>
-          {/* <EventSearchBar />
-          <EventMapDataList /> */}
+          {/* <button type="button" >測試用</button> */}
+          <EventSearchBar />
+          <EventMapDataList />
         </div>
         <div className="col-8">
           <MyMapComponent />
