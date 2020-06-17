037bd9c1060ea27fa2173cea4e167a0835441e49
Yu-Chia <l850614@gmail.com>
2020-06-17 15:03:05 +0800

    edit product&scss

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/Product.scss'
 
 import AsideBar from '../components/product/AsideBar'
+import ReactStars from 'react-rating-stars-component'
 
 function Product(props) {
   return (
@@ -46,7 +47,7 @@ function Product(props) {
                   <div className="col-sm-5 col-lg-4">
                     <img src="" />
                   </div>
-                  <div className="col-sm-7 col-lg-8">
+                  <div className="col-sm-7 col-lg-8 px-15">
                     <div className="product-detail">
                       <div className="product-label"></div>
                       <h3>
@@ -54,7 +55,11 @@ function Product(props) {
                         探索浪漫七美＆絕美藍洞＆南方四島＆雙心石滬等 |
                         自選遊覽車或機車環島
                       </h3>
-                      <p className="product-description"></p>
+                      <p className="product-description">
+                        無經驗的新手也能輕鬆做出甜點！位於台北捷運國父記念館站的Funsiamo
+                        玩美烘焙體驗，透過
+                        iPad數位教學，搭配現場服務人員的親切協助，每個走進店裡的人都能帶走精美成品與滿滿成就感，無論是自行享用或送禮都非常合適。
+                      </p>
                       <div className="product-place">
                         <i className="fas fa-map-marker-alt"></i>
                         台灣 澎湖
@@ -63,9 +68,24 @@ function Product(props) {
                         <i className="far fa-calendar"></i>
                         最早可使用日期：今日
                       </div>
-                      <div className="product-footer">
-                        <div className="product-star"></div>
-                        <span className="divider">|</span>
+                      <div className="product-footer ">
+                        <div className="product-star">
+                          <ReactStars
+                            value={3}
+                            count={5}
+                            size={20}
+                            half={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            color2={'#ffd700'}
+                            edit={false}
+                          />
+                        </div>
+                        <span className="divider"></span>
+                        <div className="product-booked-number">
+                          6.7K+ 個已訂購
+                        </div>
                         <div className="product-price">
                           <span>TWD</span>
                           <h4>130</h4>
