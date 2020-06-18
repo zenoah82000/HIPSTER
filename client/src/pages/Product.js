611339a378d2e730bf9d3581160d4fe98c9597c1
Yu-Chia <l850614@gmail.com>
2020-06-18 13:12:53 +0800

    add ProductSearchResult ProductSearchResultSort /edit product

---
+++ client/src/pages/Product.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Product(props) {
+  return <></>
+}
+
+export default Product
--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -3,6 +3,9 @@ import '../styles/ProductList.scss'
 
 import AsideBar from '../components/product/AsideBar'
 import ProductListMainContent from '../components/product/ProductListMainContent'
+import ProductSearchResult from '../components/product/ProductSearchResult'
+import ProductSearchResultSort from '../components/product/ProductSearchResultSort'
+
 import ReactStars from 'react-rating-stars-component'
 
 function ProductList(props) {
@@ -12,36 +15,8 @@ function ProductList(props) {
         <div className="row">
           <AsideBar />
           <ProductListMainContent>
-            <div className="product-result">
-              <h4>搜索結果</h4>
-            </div>
-            <div className="product-result-sort">
-              <h4>排序：</h4>
-              <span>
-                <span className="gap">|</span>
-                <a href="">
-                  <i className="far fa-thumbs-up"></i>網站推薦
-                </a>
-              </span>
-              <span>
-                <span className="gap">|</span>
-                <a href="">
-                  <i className="fab fa-hotjar"></i> 熱門程度
-                </a>
-              </span>
-              <span>
-                <span className="gap">|</span>
-                <a href="">
-                  <i className="fas fa-star"></i>用戶評價
-                </a>
-              </span>
-              <span>
-                <span className="gap">|</span>
-                <a href="">
-                  <i className="fas fa-dollar-sign"></i>價格：低到高
-                </a>
-              </span>
-            </div>
+            <ProductSearchResult />
+            <ProductSearchResultSort />
             <div className="product-list-search-info">
               <a href="">
                 <div className="row">
