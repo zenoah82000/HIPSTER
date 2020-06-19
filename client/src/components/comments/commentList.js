9e38fcc02b8e45d2473742488d95c4b91462a6b8
Yu-Chia <l850614@gmail.com>
2020-06-19 14:38:28 +0800

    edit product comment component

--- client/src/components/comments/commentList.js
+++ client/src/components/comments/commentList.js
@@ -14,160 +14,158 @@ class commtentList extends Component {
   }
   render() {
     return (
-      <div class="container m-5 ">
-        <Tabs
-          selectedIndex={this.state.tabIndex}
-          onSelect={(tabIndex) => this.setState({ tabIndex })}
-        >
-          <TabList>
-            <Tab>評價</Tab>
-            <Tab>問與答</Tab>
-          </TabList>
-          <TabPanel>
-            <div>
-              <div className="commentList d-flex row">
-                <div className="col-md-1 col-sm-2">
-                  <div className="iconBox">
-                    <img
-                      src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="commentBox col-md-11 col-sm-10">
-                  <h2 className="eventTitle">評論1 </h2>
-                  <ul className=" list-unstyled">
-                    <li>用戶名</li>
-                    <li>
-                      <RatingStar />
-                      星等
-                    </li>
-                    <li>
-                      <p className="">
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                      </p>
-                    </li>
-
-                    <li className="d-flex">
-                      <CommentImg />
-                      <CommentImg />
-                      <CommentImg />
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div className="commentList d-flex">
-                <div className="col-1">
-                  <div className="iconBox">
-                    <img
-                      src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="commentBox col-11">
-                  <h2 className="eventTitle">評論1 </h2>
-                  <ul className=" list-unstyled">
-                    <li>用戶名</li>
-                    <li>星等</li>
-                    <li>
-                      <p className="">
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                      </p>
-                    </li>
-
-                    <li className="d-flex">
-                      <CommentImg />
-                      <CommentImg />
-                      <CommentImg />
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div className="commentList d-flex">
-                <div className="col-1">
-                  <div className="iconBox">
-                    <img
-                      src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="commentBox col-11">
-                  <h2 className="eventTitle">評論1 </h2>
-                  <ul className=" list-unstyled">
-                    <li>用戶名</li>
-                    <li>星等</li>
-                    <li>
-                      <p className="">
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價是評價 這是評價 這是評價
-                        這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
-                      </p>
-                    </li>
-                    <li className="d-flex">
-                      <CommentImg />
-                      <CommentImg />
-                      <CommentImg />
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="d-flex justify-content-center   ">
-              <div className="pageButton">1</div>
-              <div className="pageButton">2</div>
-              <div className="pageButton">3</div>
-            </div>
-          </TabPanel>
-
-          <TabPanel>
-            <div>
-              <div className="eventContentBox d-flex">
-                <div className="eventImgBox col-4">
+      <Tabs
+        selectedIndex={this.state.tabIndex}
+        onSelect={(tabIndex) => this.setState({ tabIndex })}
+      >
+        <TabList>
+          <Tab>評價</Tab>
+          <Tab>問與答</Tab>
+        </TabList>
+        <TabPanel>
+          <div>
+            <div className="commentList d-flex row">
+              <div className="col-md-1 col-sm-2">
+                <div className="iconBox">
                   <img
                     src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
                     alt=""
                   />
                 </div>
-                <div className="eventDetailBox col-8 pl-3">
-                  <h2 className="eventTitle">問與答一 </h2>
-                  <ul className=" list-unstyled">
-                    <li>
-                      <div className="">手作課程</div>
-                    </li>
-                    <li>星等</li>
-                  </ul>
-                </div>
+              </div>
+              <div className="commentBox col-md-11 col-sm-10">
+                <h2 className="eventTitle">評論1 </h2>
+                <ul className=" list-unstyled">
+                  <li>用戶名</li>
+                  <li>
+                    <RatingStar />
+                    星等
+                  </li>
+                  <li>
+                    <p className="">
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                    </p>
+                  </li>
+
+                  <li className="d-flex">
+                    <CommentImg />
+                    <CommentImg />
+                    <CommentImg />
+                  </li>
+                </ul>
               </div>
             </div>
-            <div className="d-flex justify-content-center   ">
-              <div className="pageButton">1</div>
-              <div className="pageButton">2</div>
-              <div className="pageButton">3</div>
+          </div>
+
+          <div>
+            <div className="commentList d-flex">
+              <div className="col-1">
+                <div className="iconBox">
+                  <img
+                    src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="commentBox col-11">
+                <h2 className="eventTitle">評論1 </h2>
+                <ul className=" list-unstyled">
+                  <li>用戶名</li>
+                  <li>星等</li>
+                  <li>
+                    <p className="">
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                    </p>
+                  </li>
+
+                  <li className="d-flex">
+                    <CommentImg />
+                    <CommentImg />
+                    <CommentImg />
+                  </li>
+                </ul>
+              </div>
             </div>
-          </TabPanel>
-        </Tabs>
-      </div>
+          </div>
+
+          <div>
+            <div className="commentList d-flex">
+              <div className="col-1">
+                <div className="iconBox">
+                  <img
+                    src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="commentBox col-11">
+                <h2 className="eventTitle">評論1 </h2>
+                <ul className=" list-unstyled">
+                  <li>用戶名</li>
+                  <li>星等</li>
+                  <li>
+                    <p className="">
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價是評價 這是評價 這是評價
+                      這是評價 這是評價 這是評價 這是評價 這是評價 這是評價
+                    </p>
+                  </li>
+                  <li className="d-flex">
+                    <CommentImg />
+                    <CommentImg />
+                    <CommentImg />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center   ">
+            <div className="pageButton">1</div>
+            <div className="pageButton">2</div>
+            <div className="pageButton">3</div>
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <div>
+            <div className="eventContentBox d-flex">
+              <div className="eventImgBox col-4">
+                <img
+                  src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="eventDetailBox col-8 pl-3">
+                <h2 className="eventTitle">問與答一 </h2>
+                <ul className=" list-unstyled">
+                  <li>
+                    <div className="">手作課程</div>
+                  </li>
+                  <li>星等</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center   ">
+            <div className="pageButton">1</div>
+            <div className="pageButton">2</div>
+            <div className="pageButton">3</div>
+          </div>
+        </TabPanel>
+      </Tabs>
     )
   }
 }
