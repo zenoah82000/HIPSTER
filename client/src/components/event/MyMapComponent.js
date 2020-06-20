f1acdc3b5205cdf2a998f80a7cbafbd9bfa62a03
Jeanzulin <jeanzu19@gmail.com>
2020-06-20 09:53:15 +0800

    map

--- client/src/components/event/MyMapComponent.js
+++ client/src/components/event/MyMapComponent.js
@@ -56,7 +56,7 @@ export default class ViewportExample extends Component {
 
   render() {
     let { viewport, clicked } = this.props
-    console.log(viewport.center[0] - 0.001, viewport.center[0], clicked)
+    // console.log(viewport.center[0] - 0.001, viewport.center[0], clicked)
     return (
       <div>
         <Map viewport={viewport}    >
@@ -67,73 +67,140 @@ export default class ViewportExample extends Component {
 
           {clicked ?
             this.state.data.forEach((item) => {
-              if (item.lat === viewport.center[0] && item.log === viewport.center[1]) {
-                <Marker
-                  position={viewport.center}
-                  ref={this.openPopup}
-                >
-                  <Popup className="locationCard">
-                    <h5>
-                      <GiCoffeeCup className="h5 mr-1" />
-                      {item.name}
-                    </h5>
-                    <ul className="cardList list-unstyled">
-                      <li>
-                        <div className=
-                        {
-                            item.category == '咖啡廳'
-                              ? 'mapCategoryCafe'
-                              : item.category == '手作課程'
-                              ? 'mapCategoryItem'
-                              : 'mapCategoryItem2'
-                          }
-                        >
-                          <span>{item.category}</span>
-                        </div>
-                      </li>
-                      <li
-                        style={{
-                          width: '460px',
-                          height: '200px',
-                          objectFit: 'cover',
-                          overflow: 'hidden',
-                          marginTop: '10px',
-                        }}
-                      >
-                        <img
-                          src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
-                          alt=""
-                        />
-                      </li>
-                      <li>
-                        <span>星等{item.star}</span>
-                      </li>
-                      <li>
-                        <span className="mr-2">
-                          <FaRegClock />
-                        </span>
-                        <span>營業時間</span>
-                      </li>
-                      <li>
-                        <span className="mr-2">
-                          <FaMapMarkerAlt />
-                        </span>
-                        <span>地點</span>
-                      </li>
-                      <li>
-                        <span className="mr-2 ">
-                          <FaRegCalendarCheck />
-                        </span>
-                        <span>活動日期：</span>
-                      </li>
-                    </ul>
-                    <button className="cardButton" value>
-                      立即預定
-                </button>
-                  </Popup>
-                </Marker>
-              }
+              console.log(item.lat,viewport.center[0],viewport.center[1],this)
+              // item.lat === viewport.center[0] && item.log === viewport.center[1] ?
+              //   <Marker
+              //     position={viewport.center}
+              //     ref={this.openPopup}
+              //   >
+              //     <Popup className="locationCard">
+              //       <h5>
+              //         <GiCoffeeCup className="h5 mr-1" />
+              //         {item.name}
+              //       </h5>
+              //       <ul className="cardList list-unstyled">
+              //         <li>
+              //           <div className=
+              //             {
+              //               item.category == '咖啡廳'
+              //                 ? 'mapCategoryCafe'
+              //                 : item.category == '手作課程'
+              //                   ? 'mapCategoryItem'
+              //                   : 'mapCategoryItem2'
+              //             }
+              //           >
+              //             <span>{item.category}</span>
+              //           </div>
+              //         </li>
+              //         <li
+              //           style={{
+              //             width: '460px',
+              //             height: '200px',
+              //             objectFit: 'cover',
+              //             overflow: 'hidden',
+              //             marginTop: '10px',
+              //           }}
+              //         >
+              //           <img
+              //             src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+              //             alt=""
+              //           />
+              //         </li>
+              //         <li>
+              //           <span>星等{item.star}</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2">
+              //             <FaRegClock />
+              //           </span>
+              //           <span>營業時間</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2">
+              //             <FaMapMarkerAlt />
+              //           </span>
+              //           <span>地點</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2 ">
+              //             <FaRegCalendarCheck />
+              //           </span>
+              //           <span>活動日期：</span>
+              //         </li>
+              //       </ul>
+              //       <button className="cardButton" value>
+              //         立即預定
+              //   </button>
+              //     </Popup>
+              //   </Marker> :""
+              // if (item.lat === viewport.center[0] && item.log === viewport.center[1]) {
+              //   <Marker
+              //     position={viewport.center}
+              //     ref={this.openPopup}
+              //   >
+              //     <Popup className="locationCard">
+              //       <h5>
+              //         <GiCoffeeCup className="h5 mr-1" />
+              //         {item.name}
+              //       </h5>
+              //       <ul className="cardList list-unstyled">
+              //         <li>
+              //           <div className=
+              //           {
+              //               item.category == '咖啡廳'
+              //                 ? 'mapCategoryCafe'
+              //                 : item.category == '手作課程'
+              //                 ? 'mapCategoryItem'
+              //                 : 'mapCategoryItem2'
+              //             }
+              //           >
+              //             <span>{item.category}</span>
+              //           </div>
+              //         </li>
+              //         <li
+              //           style={{
+              //             width: '460px',
+              //             height: '200px',
+              //             objectFit: 'cover',
+              //             overflow: 'hidden',
+              //             marginTop: '10px',
+              //           }}
+              //         >
+              //           <img
+              //             src="https://i.pinimg.com/564x/6e/61/7c/6e617c62730ff732340ea3bf1fbef940.jpg"
+              //             alt=""
+              //           />
+              //         </li>
+              //         <li>
+              //           <span>星等{item.star}</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2">
+              //             <FaRegClock />
+              //           </span>
+              //           <span>營業時間</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2">
+              //             <FaMapMarkerAlt />
+              //           </span>
+              //           <span>地點</span>
+              //         </li>
+              //         <li>
+              //           <span className="mr-2 ">
+              //             <FaRegCalendarCheck />
+              //           </span>
+              //           <span>活動日期：</span>
+              //         </li>
+              //       </ul>
+              //       <button className="cardButton" value>
+              //         立即預定
+              //   </button>
+              //     </Popup>
+              //   </Marker>
+              // }
             }) :
+
             this.state.data.map((item) => (
               <Marker
                 position={[item.lat, item.log]}
