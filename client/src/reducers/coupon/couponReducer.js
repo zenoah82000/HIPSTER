f61917ec7d0646feb1fe6b403b8d86401324d760
Poseidoncode <41856126+ryan1095@users.noreply.github.com>
2020-06-20 22:04:05 +0800

    嘗試抓coupon資料

---
+++ client/src/reducers/coupon/couponReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux'
+
+const userCouponData = (state = {}, action) => {
+  switch (action.type) {
+    case 'GET_USERCOUPON':
+      return action.value
+    default:
+      return state
+  }
+}
+
+const couponReducer = combineReducers({
+  userCouponData,
+})
+export { couponReducer }
--- client/src/reducers/order/order_Reducers.js
+++ client/src/reducers/order/order_Reducers.js
@@ -1,20 +1,16 @@
 import { combineReducers } from 'redux'
 
-
-
 // 新增訂單回傳資料
 const memberCheckOutResponse = (state = {}, action) => {
-    switch (action.type) {
-      case 'NEW_ORDER':
-        return action.value
-      default:
-        return state
-    }
+  switch (action.type) {
+    case 'NEW_ORDER':
+      return action.value
+    default:
+      return state
   }
+}
 
-
-
-  const orderReducer = combineReducers({
-    memberCheckOutResponse,
-  })
-  export { orderReducer }
+const orderReducer = combineReducers({
+  memberCheckOutResponse,
+})
+export { orderReducer }
