f966d6edc80d0a45041dafed097a2a158e1cfa91
Yu-Chia <l850614@gmail.com>
2020-06-22 12:52:59 +0800

    add  a few product component & scss

---
+++ client/src/components/product/ProductBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/product/ProductBanner.scss'
+
+function ProductBanner(props) {
+  return (
+    <>
+      <div className="product-banner"></div>
+    </>
+  )
+}
+
+export default ProductBanner
