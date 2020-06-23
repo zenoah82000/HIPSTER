26a39eb0d06b9c1b549dfe0efb6742c4532676ca
zenoah82000 <zenoah82000@gmail.com>
2020-06-23 18:13:51 +0800

    david

--- server/src/db_connect2.js
+++ server/src/db_connect2.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "T1st@localhost",
+  password: "",
   database: "database_1",
   waitForConnections: true,
   connectionLimit: 10,
