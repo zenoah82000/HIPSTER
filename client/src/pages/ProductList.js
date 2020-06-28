b6e45f9a530c31d7ca88877c831a227f4682dc5d
Martin <l850614@gmail.com>
2020-06-28 22:31:07 +0800

    edit app.js asidebar,js productlist

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -18,12 +18,73 @@ function ProductList(props) {
   const { productListData, getProductListAsync } = props
   const searchParams = new URLSearchParams(props.location.search)
   const page = searchParams.get('page')
-  const cat = searchParams.get('cat')
+  const cat = searchParams.getAll('cat')
 
   console.log(page, cat)
+
   useEffect(() => {
     getProductListAsync()
   }, [])
+
+  const display = productListData.map((item, index) => {
+    return (
+      <>
+        <div className="product-list-search-info">
+          <a href="">
+            <div className="row">
+              <div className="col-sm-5 col-lg-4">
+                <img src="" />
+              </div>
+              <div className="col-sm-7 col-lg-8 px-15">
+                <div className="product-detail">
+                  <div className="product-label"></div>
+                  <h3>
+                    台灣澎湖跳島一日遊 |
+                    探索浪漫七美＆絕美藍洞＆南方四島＆雙心石滬等 |
+                    自選遊覽車或機車環島
+                  </h3>
+                  <p className="product-description">
+                    無經驗的新手也能輕鬆做出甜點！位於台北捷運國父記念館站的Funsiamo
+                    玩美烘焙體驗，透過
+                    iPad數位教學，搭配現場服務人員的親切協助，每個走進店裡的人都能帶走精美成品與滿滿成就感，無論是自行享用或送禮都非常合適。
+                  </p>
+                  <div className="product-place">
+                    <i className="fas fa-map-marker-alt"></i>
+                    台灣 澎湖
+                  </div>
+                  <div className="product-time">
+                    <i className="far fa-calendar"></i>
+                    最早可使用日期：今日
+                  </div>
+                  <div className="product-footer ">
+                    <div className="product-star">
+                      <ReactStars
+                        value={3}
+                        count={5}
+                        size={20}
+                        half={true}
+                        emptyIcon={<i className="far fa-star"></i>}
+                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                        fullIcon={<i className="fa fa-star"></i>}
+                        color2={'#ffd700'}
+                        edit={false}
+                      />
+                    </div>
+                    <span className="divider"></span>
+                    <div className="product-booked-number">6.7K+ 個已訂購</div>
+                    <div className="product-price">
+                      <span>TWD</span>
+                      <h4>130</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </>
+    )
+  })
 
   return (
     <>
@@ -33,6 +94,7 @@ function ProductList(props) {
           <ProductListMainContent>
             <ProductSearchResult productnumbers={productListData.length} />
             <ProductSearchResultSort />
+            {display}
             {/* -------商品列表區域------ */}
             <div className="product-list-search-info">
               <a href="">
