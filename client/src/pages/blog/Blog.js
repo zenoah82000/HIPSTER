cce1a840282cb667d121aef71e841d82a3e7761a
Faketwo <66372855+Faketwo@users.noreply.github.com>
2020-06-29 12:51:38 +0800

    BLOG

--- client/src/pages/blog/Blog.js
+++ client/src/pages/blog/Blog.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
