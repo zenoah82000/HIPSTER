fc80c3f2a199edfccf22bbe6341e5767fd142b2e
David <zenoah82000@gmail.com>
2020-07-01 12:07:12 +0800

    david

---
+++ client/src/components/home/Articles.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { FaStar, FaHeart, FaMapMarkerAlt } from 'react-icons/fa'
+import { connect } from 'react-redux'
+
+function Articles(props) {
+  const { item } = props
+
+  return (
+    <>
+      <Link to={`/blogDetail/${item.articleId}`} className="blog-group">
+        <div className="blog-image">
+          <img src={`http://localhost:5000/images/article/${item.img}`} />
+        </div>
+        <div className="blog-content">
+          <p className="blog-title">{item.articleTitle}</p>
+          <p className="blog-text">{item.articleContent}</p>
+          <div className="blog-member">
+            <div className="blog-member-photo">
+              <img
+                src={`http://localhost:5000/images/member/${item.memberImg}`}
+              />
+            </div>
+            <p className="blog-member-text">{item.memberName}</p>
+          </div>
+        </div>
+      </Link>
+    </>
+  )
+}
+
+export default Articles
---
+++ client/src/components/home/FeaturedProduct.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { FaStar, FaHeart, FaMapMarkerAlt } from 'react-icons/fa'
+import { connect } from 'react-redux'
+
+function FeaturedProduct(props) {
+  const { item } = props
+
+  // //商品區塊>關注
+  const [heart, setheart] = useState(false)
+  const heartClass = heart ? 'activity-follow active' : 'activity-follow'
+
+  //精選商品>星數顯示
+  const start1 = (
+    <>
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="start" />
+    </>
+  )
+  const start2 = (
+    <>
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+    </>
+  )
+  const start3 = (
+    <>
+      <FaStar className="" />
+      <FaStar className="" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+    </>
+  )
+  const start4 = (
+    <>
+      <FaStar className="" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+    </>
+  )
+  const start5 = (
+    <>
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+      <FaStar className="start" />
+    </>
+  )
+  const startdisplay = (value) => {
+    if (value == 1) {
+      return start1
+    } else if (value == 2) {
+      return start2
+    } else if (value == 3) {
+      return start3
+    } else if (value == 4) {
+      return start4
+    } else if (value == 5) {
+      return start5
+    }
+  }
+
+  return (
+    <>
+      <Link to="#">
+        <div className="activity-main-cont">
+          <div className="activity-picture">
+            <div
+              className={heartClass}
+              onClick={() => {
+                setheart(!heart)
+              }}
+            >
+              <FaHeart />
+            </div>
+            <img
+              src={`http://localhost:5000/images/product/${item.productImg}`}
+            />
+          </div>
+          <div className="activity-title">
+            <p>{item.productName}</p>
+          </div>
+          <div className="activity-local">
+            <p>
+              <FaMapMarkerAlt />
+              {item.productAddress}
+            </p>
+          </div>
+          <div className="home-activity-star">
+            <div className="start-group">{startdisplay(item.rating)}</div>
+          </div>
+        </div>
+      </Link>
+    </>
+  )
+}
+
+export default FeaturedProduct
