53d9548f8c8c21cd1e0932d2467b5b5c3b320be4
asd24971234 <65526219+asd24971234@users.noreply.github.com>
2020-07-02 14:55:49 +0800

    order

--- client/src/pages/mContent/UserOrder.js
+++ client/src/pages/mContent/UserOrder.js
@@ -18,8 +18,7 @@ function UserOrder() {
   const [datapage, setDatapage] = useState([])
   //讀取會員資料
   const member =JSON.parse(localStorage.getItem('member'))
-  //展開商品數量判斷
-  const [productamount,setProductamount] = useState(0)
+ 
 
   const getOrderlistAsync = async () => {
     const request = new Request(`http://localhost:5000/member/order/${member.id}`, {
@@ -102,21 +101,22 @@ function UserOrder() {
   //訂單詳情
   function Orderdetail(props) {
     return (
-      <Modal className="SignOk"
+      <Modal className="modal-border"
         {...props}
         size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Body className="SignOk-bg">
+        <Modal.Body className="modal-body">
           <div className="orderdetailbox">
-            <div>
+            <div className="ordercontact">
               <h6>聯絡人資訊</h6>
               <p>姓名:{detaildata.contact}</p>
               <p>電話:{detaildata.mobile}</p>
               <p>信箱:{detaildata.email}</p>
             </div>
-            <div>
+            <hr />
+            <div className="orderpayinfo">
               <h6>付款資訊</h6>
               <p>付款方式:</p>
               <p>小計</p>
