436df8c9ecc13e951672bb8bfa94458007844b03
Jeanzulin <jeanzu19@gmail.com>
2020-07-06 15:13:42 +0800

    push

--- client/src/pages/mContent/UserComment.js
+++ client/src/pages/mContent/UserComment.js
@@ -69,10 +69,10 @@ function UserComment(props) {
   const [text, setText] = useState('')
   const [myCommentlist, setMyCommentList] = useState([])
   const [noCommentlist, setNoCommentList] = useState([])
-
+  const member = JSON.parse(localStorage.getItem('member'))
   // 後端傳資料
   const commentAsync = async (order) => {
-    const request = new Request('http://localhost:5000/comments/2', {
+    const request = new Request(`http://localhost:5000/comments/${member.id}`, {
       method: 'get',
       // body: JSON.stringify(order),
       headers: new Headers({
