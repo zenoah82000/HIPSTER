675ea72dbf69858191438b38f6dbe041caaf0e72
Jeanzulin <jeanzu19@gmail.com>
2020-07-09 16:27:33 +0800

    update

--- server/src/productComments.js
+++ server/src/productComments.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const getProductComments = async (req) => {
   const sql =
-    "SELECT `comments`.`commentId`, `comments`.`itemListId`,`comments`.`content`, `comments`.`star`, `comments`.`commentImg`,`comments`.`created_at`, `comments`.`updated_at`,`item_lists`.`productId`,`item_lists`.`orderId`,`item_lists`.`date`,`product`.`productName`,`product`.`productImg`, `item_lists`.`memberId`,`member`.`memberName`,`member`.`memberImg` FROM `comments`LEFT JOIN `item_lists` ON `comments`.`itemListId` =`item_lists`.`itemListId` LEFT JOIN `product`ON `item_lists`.`productId` =`product`.`productId` LEFT JOIN `orderlist` ON `item_lists`.`orderId` =`orderlist`.`orderId` LEFT JOIN `member` ON `item_lists`.`memberId` = `member`.`memberId` WHERE `item_lists`.`productId` = ? ORDER BY `comments`.`updated_at` DESC";
+    "SELECT `comments`.`commentId`, `comments`.`itemListId`,`comments`.`content`, `comments`.`star`, `comments`.`commentImg`,`comments`.`created_at`, `comments`.`updated_at`,`item_lists`.`productId`,`item_lists`.`orderId`,`item_lists`.`date`,`product`.`productName`,`product`.`productImg`, `item_lists`.`memberId`,`member`.`memberName`,`member`.`memberImg` FROM `comments`LEFT JOIN `item_lists` ON `comments`.`itemListId` =`item_lists`.`itemListId` LEFT JOIN `product`ON `item_lists`.`productId` =`product`.`productId` LEFT JOIN `orderlist` ON `item_lists`.`orderId` =`orderlist`.`orderId` LEFT JOIN `member` ON `item_lists`.`memberId` = `member`.`memberId` WHERE `item_lists`.`productId` = ?  AND `comments`.`content` IS NOT NULL ORDER BY `comments`.`updated_at` DESC";
   const [result] = await db.query(sql, [req.params.productId]);
   return result;
 };
